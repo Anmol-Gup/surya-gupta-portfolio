836d9fb8394461c02ca9a6cce719bc3d4b6f0d27
Anmol-Gup <sp430661@gmail.com>
2024-05-23 21:19:54 +0530

    update navbar

--- src/components/child/Navbar.tsx
+++ src/components/child/Navbar.tsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { IoClose } from 'react-icons/io5';
 
-interface NavbarProp{
-    navColor:boolean;
+interface NavbarProp {
+    navColor: boolean;
 }
-const Navbar: React.FC<NavbarProp> = ({navColor}) => {
+const Navbar: React.FC<NavbarProp> = ({ navColor }) => {
 
-    const [toggle, setToggle] = useState<boolean>(true);
+    const [toggle, setToggle] = useState<boolean>(false);
+    const [width, setWidth] = useState<number>(innerWidth)
     const [activeLink, setActiveLink] = useState<string>('home')
 
     const handleToggle = () => {
         setToggle(!toggle);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Initial width check
+        if (width >= 1024) {
+            setToggle(true);
+        } else {
+            setToggle(false);
+        }
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [width])
+
+    console.log('i',width)
 
     const handleLink = (link: string) => {
         setActiveLink(link)
@@ -21,21 +43,21 @@ const Navbar: React.FC<NavbarProp> = ({navColor}) => {
     return (<>
         <div className="lg:hidden block px-4 py-4 basis-1/2 absolute right-0">
             {toggle ? (
-                <GiHamburgerMenu className="text-white text-2xl" onClick={handleToggle} />
-            ) : (
                 <IoClose onClick={handleToggle} className="text-2xl text-white" />
+            ) : (
+                <GiHamburgerMenu className="text-white text-2xl" onClick={handleToggle} />
             )}
         </div>
-        <nav className={`${navColor ? 'lg:bg-[#2b2d33]' : 'lg:bg-black'} lg:static basis-1/2 absolute top-full left-0 lg:top-0 w-full bg-[#2b2d33] transition-all duration-500 ease-in-out`}>
-            {!toggle && <ul className={`c text-left font-medium lg:w-full lg:h-full flex lg:flex-row lg:justify-evenly flex-col lg:gap-4 gap-2 text-white font-medium px-3 py-4`}>
+        {toggle && <nav className={`${navColor ? 'lg:bg-[#2b2d33]' : 'lg:bg-black'} lg:static basis-1/2 absolute top-full left-0 lg:top-0 w-full bg-[#2b2d33]`}>
+            <ul className={`text-left font-medium lg:w-full lg:h-full flex lg:flex-row lg:justify-evenly flex-col lg:gap-4 gap-2 text-white font-medium px-3 py-4`}>
                 <li><a href="#home" className={`list-none decoration-none ${activeLink === 'home' ? 'text-orange' : 'text-white'}`} onClick={() => handleLink('home')}>Home</a></li>
                 <li><a href="#about" className={`list-none decoration-none ${activeLink === 'about' ? 'text-orange' : 'text-white'}`} onClick={() => handleLink('about')}>About</a></li>
                 <li><a href="#projects" className={`list-none decoration-none ${activeLink === 'projects' ? 'text-orange' : 'text-white'}`} onClick={() => handleLink('projects')}>Projects</a></li>
                 <li><a href="https://myspgcom.wordpress.com/" target='_blank' className={`list-none decoration-none ${activeLink === 'blogs' ? 'text-orange' : 'text-white'}`} onClick={() => handleLink('blogs')}>Blog</a></li>
                 <li><a href="#contact" className={`list-none decoration-none ${activeLink === 'contact' ? 'text-orange' : 'text-white'}`} onClick={() => handleLink('contact')}>Contact</a></li>
                 <li className='my-3 lg:my-0'><a href='../../src/assets/Surya_P__Gupta.pdf' className='list-none decoration-none border-2 border-orange rounded-full px-3 py-2 transition-all ease cv' download>Download CV</a></li>
-            </ul>}
-        </nav>
+            </ul>
+        </nav>}
     </>)
 }
 
