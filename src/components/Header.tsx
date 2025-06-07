409823cb6a93cc19c01f31c155f2804cff9b4a50
Surya Prakash Gupta <66009201+Anmol-Gup@users.noreply.github.com>
2025-06-07 12:13:34 +0530

    Update Header.tsx
    set header color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header: React.FC = () => {
   return (
     <>
       <Progress />
-      <header className={`sticky top-0 z-40 w-full lg:px-10 xl:px-20 lg:py-3 px-6 py-4 items-center flex justify-between bg-[#2b2d33] ${navColor ? 'lg:bg-[#2b2d33]' : 'lg:bg-black'}`}>
+      <header className={`sticky top-0 z-40 w-full lg:px-10 xl:px-20 lg:py-3 px-6 py-4 items-center flex justify-between bg-[#2b2d33] ${navColor ? 'lg:bg-[#2b2d33]' : 'lg:bg-[#0b132b]'}`}>
         <h1 className="text-3xl md:text-4xl font-medium text-white md:basis-1/2">Surya</h1>
         <Navbar navColor={navColor}/>
       </header>
